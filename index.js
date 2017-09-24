36a8bfb2e1462b1b8f1165946db06caecbfa2407
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-24 09:39:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var takeANumber = function(katzDeliLine,personName){
+  katzDeliLine.push(personName);
+  for(var i = 0; i < katzDeliLine.length;i++){
+    var positionOnLine = "Welcome, " + katzDeliLine[i] + ". You are number " + parseInt(i + 1) + " in line.";
+  }
+  return positionOnLine
+}
+
+var nowServing = function(katzDeliLine){
+  if(katzDeliLine.length <= 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    for(var i = 0; i <katzDeliLine.length; i++){
+      var newStatement = "Currently serving " + katzDeliLine[i] + ".";
+      katzDeliLine.shift();
+      return newStatement;
+    }
+  }
+}
